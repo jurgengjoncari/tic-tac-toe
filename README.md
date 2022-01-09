0f821a1b3540914ac69d767767cc098f9b56b8f6
Jurgen <jurgen.gjoncari@gmail.com>
2022-01-09 16:48:25 +0100

    Fix some things

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tic Tac Toe
+
+This is the Tic-tac-toe game from [The Odin Project Full-stack JavaScript course](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/tic-tac-toe).
+
+The game is played between two users. In the future, I'll implement the option to play against the AI to.
--- script1.js
+++ script1.js
@@ -1,88 +1,86 @@
-let Game = (function () {
-    let board = Array.from(Array(3), () => [undefined, undefined, undefined]);
-    
+let Gameboard = (function () {
+    let board = [[]]
+
     let diagonals = {
-        left: board.map((row, i) => row.find((cell, j) => i = j)),
-        right: board.map((row, i) => row.find((cell, j) => i = 2 - j))
-    };
+        left: board.map((row, i) => row.find((cell, j) => (i = j))),
+        right: board.map((row, i) => row.find((cell, j) => (i = 2 - j))),
+    }
 
     function getRows(position) {
         return {
             horizontal: board[position.row],
-            vertical: board.map(row => row[position.col])
+            vertical: board.map((row) => row[position.col]),
         }
-    };
+    }
 
-    board.oneRowFilled = function(mark, position) {
+    board.oneRowFilled = function (mark, position) {
         rows = [
-            getRows(position).horizontal, 
-            getRows(position).vertical, 
-            diagonals.left, 
-            diagonals.right
-        ];
+            getRows(position).horizontal,
+            getRows(position).vertical,
+            diagonals.left,
+            diagonals.right,
+        ]
 
-        let isMarked = (row) => row.every(cell => cell == mark);
-        let areMarked = (rows) => rows.map(row => isMarked(row));
-        
-        if (areMarked(rows).some(Boolean)) return true;
-        else return false;
+        let isMarked = (row) => row.every((cell) => cell == mark)
+        let areMarked = (rows) => rows.map((row) => isMarked(row))
+
+        if (areMarked(rows).some(Boolean)) return true
+        else return false
     }
-    
+
     board.isDraw = function () {
-        let isEmpty = (cell) => cell === undefined;
-        if (board.some(row => row.some(isEmpty(cell)))) return false;
-        else return true;
+        let isEmpty = (cell) => cell === undefined
+        if (board.some((row) => row.some(isEmpty(cell)))) return false
+        else return true
     }
 
-    return board;
-})();
+    return board
+})()
 
 function createPlayer(mark) {
-    let name = prompt("Enter your name: ");
-
-    let position = {};
-
+    let name = prompt('Enter your name: ')
+    let position = {}
     let setPosition = (row, col) => {
-        position.row = row;
-        position.col = col;
+        position.row = row
+        position.col = col
     }
-
-    let won = (board) => board.oneRowFilled(mark, position);
+    let won = (board) => board.oneRowFilled(mark, position)
 
     return {
-        mark, 
-        name, 
-        setPosition, 
-        won
-    };
-};
-
-let playerX = createPlayer("X");
-let playerO = createPlayer("O");
-
-let playGame = function(player) {
-    let row = prompt(`Please enter an empty row: `); 
-    let col = prompt(`Please enter an empty column: `);
-
-    if (Game[row][col] == undefined) {
-        Game[row][col] = player.mark;
-        player.setPosition(row, col)
+        mark,
+        name,
+        setPosition,
+        won,
     }
-    else playGame(player);
+}
 
-    let isOver = (game) => player.won(game) || game.isDraw();
-    
-    let changeTurn = (player) => (player == playerO) ? playerX : playerO;
+let playerX = createPlayer('X')
+let playerO = createPlayer('O')
+
+let playGame = function (player) {
+    let row = prompt(`Please enter an empty row: `)
+    let col = prompt(`Please enter an empty column: `)
+
+    if (Gameboard[row] == undefined) {
+        Gameboard[row] = []
+        if (Gameboard[row][col] == undefined) {
+            Gameboard[row][col] = player.mark
+            player.setPosition(row, col)
+        }
+    } else playGame(player)
+
+    let isOver = (game) => player.won(game) || game.isDraw()
+
+    let changeTurn = (player) => (player == playerO ? playerX : playerO)
 
     let restart = () => {}
 
-    if (isOver(Game)) {
-        if (player.won(Game)) console.log(`${player.name} is winner`);
-        else if (Game.isDraw()) console.log("It's a draw");
-        restart();
+    if (isOver(Gameboard)) {
+        if (player.won(Gameboard)) console.log(`${player.name} is winner`)
+        else if (Gameboard.isDraw()) console.log("It's a draw")
+        restart()
         return
-    } 
-    else playGame(changeTurn(player));
-};
+    } else playGame(changeTurn(player))
+}
 
-playGame(playerX);
+playGame(playerX)
